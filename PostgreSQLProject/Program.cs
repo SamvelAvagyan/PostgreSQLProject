bc84ca40772d8397ad508f319618d077c0b1aff7
avagyansamvel <avagyansamvel07@gmail.com>
2022-07-28 12:34:11 +0400

    Add InsertUsers method

--- PostgreSQLProject/Program.cs
+++ PostgreSQLProject/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Npgsql;
 using System;
+using System.Data;
 
 namespace PostgreSQLProject
 {
@@ -7,12 +8,26 @@ namespace PostgreSQLProject
     {
         static void Main(string[] args)
         {
+            InsertUsers(1000);
+        }
 
+        static void InsertUsers(int count)
+        {
+            using (NpgsqlConnection con = GetConnection())
+            {
+                con.Open();
+                for (int i = 0; i < count; i++)
+                {
+                    string query = $"insert into public.Users(FirstName, LastName, Email) values('A{i}', 'A{i}yan', 'A{i}@gmail.com')";
+                    NpgsqlCommand command = new NpgsqlCommand(query, con);
+                    command.ExecuteNonQuery();
+                }
+            }
         }
 
         static NpgsqlConnection GetConnection()
         {
-            return new NpgsqlConnection(@"Server=localhost;Port=5432;User Id=postgres;Password=root1234;Database=myDB;");
+            return new NpgsqlConnection(@"Server=localhost;Port=5432;User Id=postgres;Password=root1234;Database=myDB");
         }
     }
 }
