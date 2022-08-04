1da8ab681cd7df676ca3da6fe29cbffb3f00f6f9
avagyansamvel <avagyansamvel07@gmail.com>
2022-08-04 11:51:01 +0400

    Add DbContext and InsertUsers

--- PostgreSQLProject/Program.cs
+++ PostgreSQLProject/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 using System.Threading;
 
 namespace PostgreSQLProject
@@ -9,70 +10,46 @@ namespace PostgreSQLProject
     internal class Program
     {
         private static List<User> users;
+        private static UserDbContext dbContext = new UserDbContext();
+
         static void Main(string[] args)
         {
-            //InsertUsers(1000);
+            InsertUsers(1000);
             //UpdateUser(1, new User("A0", "+37498264752", DateTime.Now));
             //List<User> users = ReadData();
-            Thread thread = new Thread(FirstQuery);
-            Thread thread1 = new Thread(SecondQuery);
-            thread.Start();
-            thread1.Start();
+            //Thread thread = new Thread(FirstQuery);
+            //Thread thread1 = new Thread(SecondQuery);
+            //thread.Start();
+            //thread1.Start();
         }
 
         static void FirstQuery()
         {
             users = ReadData();
+            for (int i = 0; i < users.Count; i++)
+            {
+                Console.Write(users[i].Name + ", ");
+            }
         }
 
         static void SecondQuery()
         {
-            UpdateUser(35, new User("A30", "+37456251836", DateTime.Now));
+            UpdateUser(12, new User("A30", "+37456251836", DateTime.Now));
         }
 
         static void InsertUsers(int count)
         {
-            using (NpgsqlConnection con = GetConnection())
+            Random rnd = new Random();
+            for (int i = 0; i < count; i++)
             {
-                con.Open();
-                Random rnd = new Random();
-                for (int i = 0; i < count; i++)
-                {
-                    User user = new User($"A{i}", $"+374{rnd.Next(90000000, 99999999)}", DateTime.Now);
-                    AddUser(user);
-                }
-            }
-        }
-
-        static void AddUser(User user)
-        {
-            using (NpgsqlConnection con = GetConnection())
-            {
-                con.Open();
-                string query = $"insert into public.Users(Name, Number, Date) values('{user.Name}', '{user.Number}', '{user.Date}')";
-                NpgsqlCommand command = new NpgsqlCommand(query, con);
-                command.ExecuteNonQuery();
+                User user = new User($"A{i}", $"+374{rnd.Next(90000000, 99999999)}", DateTime.Now);
+                dbContext.Users.Add(user);
             }
         }
 
         static List<User> ReadData()
         {
-            List<User> users = new List<User>();
-            using (NpgsqlConnection con = GetConnection())
-            {
-                string query = "select * from Users";
-                con.Open();
-                NpgsqlCommand command = new NpgsqlCommand(query, con);
-                NpgsqlDataReader reader = command.ExecuteReader();
-
-                while (reader.Read())
-                {
-                    User user = ReadUser(reader);
-                    users.Add(user);
-                }
-            }
-
-            return users;
+            return dbContext.Users.ToList();
         }
 
         static User ReadUser(NpgsqlDataReader reader)
---
+++ PostgreSQLProject/UserDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PostgreSQLProject
+{
+    class UserDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Server=localhost;Port=5432;User Id=postgres;Password=root1234;Database=myDB");
+        }
+    }
+}
